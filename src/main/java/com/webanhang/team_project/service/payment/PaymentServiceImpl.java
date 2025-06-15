e1c0ef1e374c440544f878dff7ba189c8147b4d2
Sang Virgo <tansang06092004@gmail.com>
2025-06-16 01:31:19 +0700

    fix(project): fix vnp

--- src/main/java/com/webanhang/team_project/service/payment/PaymentServiceImpl.java
+++ src/main/java/com/webanhang/team_project/service/payment/PaymentServiceImpl.java
@@ -43,6 +43,9 @@ public class PaymentServiceImpl implements PaymentService {
     @Value("${vnpay.return-url}")
     private String vnp_Returnurl;
 
+    // @Value("${vnpay.ipn-url}")
+    // private String vnp_IpnUrl;
+
     private final IOrderService orderService;
 
     private final PaymentRepository paymentRepository;
@@ -84,6 +87,7 @@ public class PaymentServiceImpl implements PaymentService {
             // Sửa Return URL - không đính kèm orderId vào URL
             vnp_Params.put("vnp_ReturnUrl", vnp_Returnurl);
             vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+            // vnp_Params.put("vnp_IpnUrl", vnp_IpnUrl);
 
             ZoneId vietnamZoneId = ZoneId.of("Asia/Ho_Chi_Minh");
 
